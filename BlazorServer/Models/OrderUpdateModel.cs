15b039c06141f5ab77a32a44328843007e6feb16
ssrout <smruti.sambit.rout@gmail.com>
2020-08-31 00:00:44 +0530

    Blazor server app added

---
+++ BlazorServer/Models/OrderUpdateModel.cs
@@ -0,0 +1,21 @@
+﻿using DataLibrary.Models;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace BlazorServer.Models
+{
+    public class OrderUpdateModel
+    {
+        public int Id { get; set; }
+
+        [Required]
+        [MaxLength(20, ErrorMessage = "max 20 char")]
+        [MinLength(3, ErrorMessage = "min 3 char")]
+        [DisplayName("Name For The Order")]
+        public string OrderName { get; set; }
+    }
+}
