8fa93071f04d8527dc0934cb2098c9a08ead74f8
ssrout <smruti.sambit.rout@gmail.com>
2020-08-31 20:49:45 +0530

    webassembly added

--- BlazorClient/Models/OrderUpdateModel.cs
+++ BlazorClient/Models/OrderUpdateModel.cs
@@ -1,5 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel;
+using System.ComponentModel.DataAnnotations;
 using System.Linq;
 using System.Threading.Tasks;
 
@@ -8,6 +10,10 @@ namespace BlazorClient.Models
     public class OrderUpdateModel
     {
         public int Id { get; set; }
+        [Required]
+        [MaxLength(20, ErrorMessage = "max 20 char")]
+        [MinLength(3, ErrorMessage = "min 3 char")]
+        [DisplayName("Name For The Order")]
         public string OrderName { get; set; }
     }
 }
